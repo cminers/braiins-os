7c0e1358ac2a36dae75160f21a594ce9d5b2aaea
Libor Vašíček <libor.vasicek@braiins.com>
2019-05-29 16:21:50 +0200

    Clean LEDE build directory before actual firmware build
    - add 'clean' command before feeds update to ensure that system rootfs
      wont be corrupted with some garbage from previous build

--- build-release.sh
+++ build-release.sh
@@ -107,6 +107,7 @@ for subtarget in $release_subtargets; do
     # We need to ensure that feeds are update
     if [ $STAGE1 = y ]; then
 	$DRY_RUN ./bb.py --platform $platform prepare
+	$DRY_RUN ./bb.py --platform $platform clean
 	$DRY_RUN ./bb.py --platform $platform prepare --update-feeds
 	# build everything for a particular platform
 	$DRY_RUN ./bb.py --platform $platform build --key $key -j$parallel_jobs -v
